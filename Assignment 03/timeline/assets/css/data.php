f5b5cb9f2807f0e0376bf87883e761f8fdf16433
taymurahmed <34242361+taymurahmed@users.noreply.github.com>
2022-03-23 15:39:59 +0600

    Add files via upload

---
+++ Assignment 03/timeline/assets/css/data.php
@@ -0,0 +1,61 @@
+<?php
+
+
+
+$posts = [
+    [
+        'pro_pic' => 'https://i0.wp.com/bdamajanshop.com/wp-content/uploads/2018/08/team-member-1.jpg?fit=370%2C280&ssl=1',
+        'name' => 'Taymur Ahmed',
+        'time' => '2 mins ago',
+        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quod suscipit quibusdam maxime neque laudantium ab hic molestias placeat nisi dolore!',
+        'timeline_pic' => 'https://bestcoverpix.com/wp-content/uploads/2013/10/84.jpg',
+        'likes' => '120',
+        'comments' => '190 Comments'
+    ],
+    [
+        'pro_pic' => 'https://www.brunel.ac.uk/research/Projects/images/muslim-boy.x7ba6bafd.jpg?q=70&f=webp&Width=330&Height=330&w=525&h=350&crop=350,350,88,0',
+        'name' => 'Fazal Karim',
+        'time' => '9 mins ago',
+        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quod suscipit quibusdam maxime neque laudantium ab hic molestias placeat nisi dolore!',
+        'timeline_pic' => 'https://i.pinimg.com/originals/b3/9f/d8/b39fd8fd5ac2e8c25938e2fd1783d016.jpg',
+        'likes' => '633',
+        'comments' => '555 Comments'
+    ],
+    [
+        'pro_pic' => 'https://www.muslimwomenscouncil.org.uk/sites/default/files/articles/A%20restaurant%20manager%20was%20fired%20for%20removing%20the%20Hijab%20of%20a%20Muslim%20employee.jpg',
+        'name' => 'Afreen Khatun',
+        'time' => '33 mins ago',
+        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quod suscipit quibusdam maxime neque laudantium ab hic molestias placeat nisi dolore!',
+        'timeline_pic' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJDv85AyH6ZHeKS_L1SfjoLN-3pLb4jhCs52ATFx0JdNuZIMcE5bMdN-tYK6sM0vh35AE&usqp=CAU',
+        'likes' => '893',
+        'comments' => '201 Comments'
+    ],
+    [
+        'pro_pic' => 'https://youth.imsa.us/images/demo/Teams/Team4.jpg',
+        'name' => 'Bashir Ahmad',
+        'time' => '2 days ago',
+        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quod suscipit quibusdam maxime neque laudantium ab hic molestias placeat nisi dolore!',
+        'timeline_pic' => 'https://i.pinimg.com/736x/d6/e9/11/d6e9119786592549eaa470d83cb05d80.jpg',
+        'likes' => '256',
+        'comments' => '53 Comments'
+    ],
+    [
+        'pro_pic' => 'https://us.123rf.com/450wm/vital9c/vital9c2101/vital9c210100225/162169364-young-woman-in-arab-headscarf-a-woman-is-looking-at-the-camera-against-the-background-of-the-arab-ho.jpg?ver=6',
+        'name' => 'Maria Gomez',
+        'time' => '3 days ago',
+        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quod suscipit quibusdam maxime neque laudantium ab hic molestias placeat nisi dolore!',
+        'timeline_pic' => 'https://cdn.statically.io/img/timelinecovers.pro/facebook-cover/download/life-facebook-cover.jpg',
+        'likes' => '1200',
+        'comments' => '263 Comments'
+    ]
+
+
+    ];
+
+
+
