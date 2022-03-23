066eb5e0a0d03939fbf8f86f62267423d6f5e475
taymurahmed <34242361+taymurahmed@users.noreply.github.com>
2022-03-23 15:35:26 +0600

    Add files via upload

---
+++ Assignment 03/Mobile/data.php
@@ -0,0 +1,60 @@
+<?php
+
+
+
+$devs = [
+    [
+        'name' => 'Primo H10',
+        'skill' => '12999/=',
+        'desc' => '
+        Operating System	Android 11™
+        Processor	        2.3GHz Octa-Core | Helio G35 Gaming Processor
+        GPU	                IMG PowerVR GE8320
+        RAM	                4GB/6GB
+        Storage (ROM)	    64GB
+        Expandable Memory	Up to 256GB',
+        'photo' => 'https://waltonbd.com/image/catalog/category-banner/mobile/smart-phone-half-block.jpg'
+    ],
+    [
+        'name' => 'S 17',
+        'skill' => '14500',
+        'desc' => 'Operating System	Android 11™
+        Processor	        2.3GHz Octa-Core | Helio G35 Gaming Processor
+        GPU	                IMG PowerVR GE8320
+        RAM	                4GB/6GB
+        Storage (ROM)	    64GB
+        Expandable Memory	Up to 256GB',
+        'photo' => 'https://www.itel-mobile.com/fileadmin/assets/img/product/S17/detail_page/itel-S17_________-__________01.jpg'
+    ],
+    [
+        'name' => 'Oppo Reno 7',
+        'skill' => '24999/=',
+        'desc' => 'Operating System	Android 11™
+        Processor	        2.3GHz Octa-Core | Helio G35 Gaming Processor
+        GPU	                IMG PowerVR GE8320
+        RAM	                4GB/6GB
+        Storage (ROM)	    64GB
+        Expandable Memory	Up to 256GB',
+        'photo' => 'https://www.thechennaimobiles.com/image/cache/catalog/opo71-600x600.jpg'
+    ],
+    [
+        'name' => 'Poco X3',
+        'skill' => '28950/=',
+        'desc' => 'Operating System	Android 11™
+        Processor	        2.3GHz Octa-Core | Helio G35 Gaming Processor
+        GPU	                IMG PowerVR GE8320
+        RAM	                4GB/6GB
+        Storage (ROM)	    64GB
+        Expandable Memory	Up to 256GB',
+        'photo' => 'https://imgk.timesnownews.com/story/iPhone_Xs_79_0.png?tr=w-1200,h-900'
+    ]
+  
+
+];
+
+/*
+foreach($devs as $dev){
+    echo "<img style=\" width:200px; height:200px \" src=\"".$dev['photo']."\">";
+    echo $dev['name'];
+    echo "<hr>";
+}*/
